edb5c99c790b383080c8c1e4a64c5225269f6711
An <antr.2201@gmail.com>
2020-10-21 12:54:23 +0200

    Add scene

---
+++ Assets/Scripts/FresnelMatController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FresnelMatController : MonoBehaviour
+{
+    public float maxAmount = 2.4f;
+    public float originalAmount = 1.2f;
+    public float amount = 1.2f;
+    public float step = 0.02f;
+    public float speed = 1.0f;
+
+    private MeshRenderer meshRenderer;
+
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (amount >= maxAmount)
+        {
+            amount = originalAmount;
+        } else
+        {
+            amount += step;
+        }
+
+        amount = Mathf.Lerp(amount, 0, Time.deltaTime * speed);
+        meshRenderer.material.SetFloat("_Amount", amount);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            amount += step;
+        }
+    }
+}
---
+++ Assets/Scripts/VertexDisplacementController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VertexDisplacementController : MonoBehaviour
+{
+    public float displacementAmount = 0.6f;
+    public float initialAmount = 0.6f;
+    public float displacementStep = 0.2f;
+    public float speed = 2f;
+    public bool interactable = true;
+
+    private MeshRenderer meshRenderer;
+
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (displacementAmount < 0.1)
+        {
+            displacementAmount += displacementStep;
+        }
+
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime * speed);
+        meshRenderer.material.SetFloat("_Amount", displacementAmount);
+
+        if (interactable && Input.GetKey(KeyCode.Space))
+        {
+            displacementAmount += displacementStep;
+        }
+    }
+
+}
